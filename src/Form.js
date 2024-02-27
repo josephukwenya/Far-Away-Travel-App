ad7976195bfc4cbea7960eb38abbef28843f734b
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-02-27 18:20:25 +0100

    initial commit

---
+++ src/Form.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newForm = { description, quantity, packed: false, id: Date.now() };
+    // console.log(newForm)
+    onAddItems(newForm);
+
+    setDescription('');
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit} alt='form' name="form">
+      <h3>What do you need for your 😍 trip?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+      </select>
+      <input type="text" value={description} onChange={e => setDescription(e.target.value)} placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+}
